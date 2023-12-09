a9bdfb2e8195936b040b3905280a4c118c90edae
nomayen <nomayen.ohin@gmail.com>
2023-12-09 12:30:12 +0600

    server to data fatching and show ui

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,12 @@ function App() {
         body: JSON.stringify(user)
     })
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+      console.log(data)
+      const newUsers = [...users , data];
+      setUsers(newUsers);
+      form.reset();
+    })
    
   }
   useEffect(()=>{
